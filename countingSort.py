62fb1b605d300667ae26ddbd4631c587c3861cda
cjn <cjnbill@qq.com>
2015-12-11 16:36:58 +0800

    add test

--- countingSort.py
+++ countingSort.py
@@ -6,12 +6,13 @@ def COUNTING_SORT(A,B,k):
         C[A[j]]=C[A[j]]+1
     for i in range(1,k+1):
         C[i]=C[i]+C[i-1]
+    print C
     for j in range(len(A)-1,-1,-1):
-        B[C[A[j]]]=A[j]
+        B[C[A[j]]-1]=A[j]
         C[A[j]]=C[A[j]]-1
 
 if __name__ == '__main__':
     A=genarray(0,100,100)
-    B=[0]*101
+    B=[0]*100
     COUNTING_SORT(A,B,100)
     print B
---
+++ test.py
@@ -0,0 +1,9 @@
+__author__ = 'cuijianing'
+from base import *
+import heapSort
+import countingSort
+import quicksort
+import Sort
+if __name__ == '__main__':
+    A=genarray(0,100,100)
+    heapSort.HEAPSORT(A)
